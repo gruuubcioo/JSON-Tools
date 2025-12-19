91756fdfdb78a45feaf0ff41f414eff14ec0ab3b
husarixxx <janekdebest21@gmail.com>
2025-12-19 20:09:50 +0100

    created a compare service and logic to compare lines in json and connets it's to the New REST endpoint which was created

---
+++ src/main/java/pl/put/poznan/transformer/rest/JsonCompareController.java
@@ -0,0 +1,29 @@
+package pl.put.poznan.transformer.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.put.poznan.transformer.logic.JsonComparatorService;
+import pl.put.poznan.transformer.logic.JsonComparer;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/compare/json")
+public class JsonCompareController {
+
+    private final JsonComparatorService jsonComparatorService;
+
+    @Autowired
+    public JsonCompareController(JsonComparatorService jsonComparatorService) {
+        this.jsonComparatorService = jsonComparatorService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public List<Integer> post(@RequestBody JsonComparer compare) {
+
+        return jsonComparatorService.compareJsonLineByLine(compare.getFirstJson(), compare.getSecondJson());
+    }
+}
