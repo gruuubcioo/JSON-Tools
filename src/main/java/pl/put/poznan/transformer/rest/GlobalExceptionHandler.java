75fc3864ee014ba915c26bd70f4d275744378ef9
gruuubyy <wiktordzidziul@gmail.com>
2025-12-19 19:36:52 +0100

    Fix GlobalExceptionHandler.java by replacing String with ResponseEntity

--- src/main/java/pl/put/poznan/transformer/rest/GlobalExceptionHandler.java
+++ src/main/java/pl/put/poznan/transformer/rest/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package pl.put.poznan.transformer.rest;
 
 import org.springframework.http.HttpStatus;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public String handleIllegalArgument(IllegalArgumentException e) {
-        return "Error blad danych  : " + e.getMessage() + "\n" + " status : " + HttpStatus.BAD_REQUEST.value();
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        String message = "Error blad danych: " + e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public String handleIllegalArgument(Exception e) {
-        return "Error blad serwera  : " + e.getMessage() + "\n" + "status błędu: " + HttpStatus.BAD_REQUEST.value();
+    public ResponseEntity<String> handleGeneralException(Exception e) {
+        String message = "Error blad serwera: " + e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 }
