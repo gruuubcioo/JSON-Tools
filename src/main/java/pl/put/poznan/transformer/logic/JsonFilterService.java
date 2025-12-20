d8b9d98097394a7b552477f4b9fa485181fb9d61
Norbert Dzudzewicz <norbert.dzudzewicz@student.put.poznan.pl>
2025-12-20 23:31:55 +0100

    Logika iterujaca po strukturze JSON i usuwajaca wskazane pola #16

--- src/main/java/pl/put/poznan/transformer/logic/JsonFilterService.java
+++ src/main/java/pl/put/poznan/transformer/logic/JsonFilterService.java
@@ -2,27 +2,30 @@ package pl.put.poznan.transformer.logic;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.List;
 
 public class JsonFilterService {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // Metoda dla #15, przyjmuje tekst jako String i liste pól do usunięcia
     public String filterJson(String jsonInput, List<String> keys){
         try{
-            //
-            JsonNode initNode = objectMapper.readTree(jsonInput);
 
-            //Inicjacja logiki usuwania niechcianych pól
-            JsonNode filteredNode = processNode(initNode, keys);
+            JsonNode initNode = objectMapper.readTree(jsonInput);
+            JsonNode filteredNode = deleteKeys(initNode, keys);
             return filteredNode.toPrettyString();
         }catch (Exception e){
             throw new IllegalArgumentException("Error: Przesłany tekst nie jest w formacie JSON");
         }
     }
-    public JsonNode processNode(JsonNode node, List<String> keys){
-        
+    public JsonNode deleteKeys(JsonNode node, List<String> keys){
+        if(node.isObject()){
+            ObjectNode objectNode = (ObjectNode) node;
+            objectNode.remove(keys);
+        }
+        //Rekurencja
+        node.forEach(child -> deleteKeys(child,keys));
         return node;
     }
 }
