b9604942572d0c19028d2298554c1a543fd7c742
Norbert Dzudzewicz <norbert.dzudzewicz@student.put.poznan.pl>
2025-12-21 00:36:03 +0100

    Obsluga przypadku, gdy podany klucz nie istnieje lub jest bledny #17

--- src/main/java/pl/put/poznan/transformer/logic/JsonFilterService.java
+++ src/main/java/pl/put/poznan/transformer/logic/JsonFilterService.java
@@ -1,31 +1,54 @@
-package pl.put.poznan.transformer.logic;
+    package pl.put.poznan.transformer.logic;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+    import com.fasterxml.jackson.databind.JsonNode;
+    import com.fasterxml.jackson.databind.ObjectMapper;
+    import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import java.util.List;
+    import java.util.ArrayList;
+    import java.util.List;
 
-public class JsonFilterService {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    public class JsonFilterService {
+        private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public String filterJson(String jsonInput, List<String> keys){
-        try{
+        public String filterJson(String jsonInput, List<String> keys){
+            try{
+                List<String> validKeys = new ArrayList<>();
+                for(String key : keys){
+                    if(key != null && !key.trim().isEmpty()){
+                        validKeys.add(key);
+                    }
+                    else{
+                        //Informuj ze wykryto bledny klucz (do doknczenia przy #18)
+                    }
+                }
+                List<String> foundKeys = new ArrayList<>();
 
-            JsonNode initNode = objectMapper.readTree(jsonInput);
-            JsonNode filteredNode = deleteKeys(initNode, keys);
-            return filteredNode.toPrettyString();
-        }catch (Exception e){
-            throw new IllegalArgumentException("Error: Przesłany tekst nie jest w formacie JSON");
+                JsonNode initNode = objectMapper.readTree(jsonInput);
+
+                JsonNode filteredNode = deleteKeys(initNode, keys, foundKeys);
+                for(String key : validKeys){
+                    if(!foundKeys.contains(key)){
+                        //Informuj ze klucz nie zostal znaleziony (do doknczenia przy #18)
+                    }
+                }
+
+                return filteredNode.toPrettyString();
+            }catch (Exception e){
+                throw new IllegalArgumentException("Error: Przesłany tekst nie jest w formacie JSON");
+            }
+        }
+        public JsonNode deleteKeys(JsonNode node, List<String> keys, List<String> foundKeys){
+            if(node.isObject()){
+                ObjectNode objectNode = (ObjectNode) node;
+                for(String key : keys){
+                    if(objectNode.has(key)){
+                        objectNode.remove(key);
+                        foundKeys.add(key);
+                    }
+                }
+            }
+            //Rekurencja
+            node.forEach(child -> deleteKeys(child,keys,foundKeys));
+            return node;
         }
     }
-    public JsonNode deleteKeys(JsonNode node, List<String> keys){
-        if(node.isObject()){
-            ObjectNode objectNode = (ObjectNode) node;
-            objectNode.remove(keys);
-        }
-        //Rekurencja
-        node.forEach(child -> deleteKeys(child,keys));
-        return node;
-    }
-}
