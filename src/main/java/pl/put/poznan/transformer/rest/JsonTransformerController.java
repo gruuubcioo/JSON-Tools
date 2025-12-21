791282c53cc640bd6a09c637cadc0ac9de013b92
tomaszdwoznik <tomek.dwoznik@icloud.com>
2025-12-21 17:27:58 +0100

    Modified REST controller to work with both minify and filter functions

--- src/main/java/pl/put/poznan/transformer/rest/JsonTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/JsonTransformerController.java
@@ -3,15 +3,16 @@ package pl.put.poznan.transformer.rest;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.JsonTransformer;
+import java.util.List;
 
 @RestController
 @RequestMapping("/transform/json")
 public class JsonTransformerController {
 
     @RequestMapping(method = RequestMethod.POST, produces = "text/plain")
-    public String post(@RequestParam(value = "transforms") String[] transforms, @RequestBody JsonNode body) {
+    public String post(@RequestParam(value = "transforms") String[] transforms, @RequestParam(value = "keys", required = false) List<String> keys, @RequestBody JsonNode body) {
 
-        JsonTransformer transformer = new JsonTransformer(transforms);
+        JsonTransformer transformer = new JsonTransformer(transforms, keys);
         return transformer.transform(body);
     }
 }
