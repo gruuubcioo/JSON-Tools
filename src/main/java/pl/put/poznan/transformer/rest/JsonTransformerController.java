81a9e0f3ef56b0163df730e155e352dce13594f0
tomaszdwoznik <tomek.dwoznik@icloud.com>
2025-12-21 16:24:49 +0100

    Adjusted data type of post method

--- src/main/java/pl/put/poznan/transformer/rest/JsonTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/JsonTransformerController.java
@@ -2,20 +2,18 @@ package pl.put.poznan.transformer.rest;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.JsonComparer;
 import pl.put.poznan.transformer.logic.JsonTransformer;
 
 @RestController
 @RequestMapping("/transform/json")
 public class JsonTransformerController {
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public JsonNode post(@RequestParam(value = "transforms") String[] transforms, @RequestBody JsonNode body) {
+    @RequestMapping(method = RequestMethod.POST, produces = "text/plain")
+    public String post(@RequestParam(value = "transforms") String[] transforms, @RequestBody JsonNode body) {
 
         JsonTransformer transformer = new JsonTransformer(transforms);
         return transformer.transform(body);
     }
-
 }
 
 
