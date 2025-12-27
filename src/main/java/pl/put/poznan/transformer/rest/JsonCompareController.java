6e3b1c4b78b97095d7de96019eecedadc11b384d
tomaszdwoznik <tomek.dwoznik@icloud.com>
2025-12-27 16:34:19 +0100

    Rebuild appliaction structure

--- src/main/java/pl/put/poznan/transformer/rest/JsonCompareController.java
+++ src/main/java/pl/put/poznan/transformer/rest/JsonCompareController.java
@@ -1,15 +1,13 @@
 package pl.put.poznan.transformer.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.JsonComparatorService;
 import pl.put.poznan.transformer.logic.JsonComparer;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/compare/json")
 public class JsonCompareController {
--- src/main/java/pl/put/poznan/transformer/rest/JsonTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/JsonTransformerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.JsonTransformer;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/transform/json")
 public class JsonTransformerController {
