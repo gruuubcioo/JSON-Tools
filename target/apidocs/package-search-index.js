16aa5185455c79c1b7d632a15657d346db4fb4d7
gruuubyy <wiktordzidziul@gmail.com>
2025-12-30 12:55:27 +0100

    Delete target folder

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.put.poznan.JsonTools.app"},{"l":"pl.put.poznan.JsonTools.logic"},{"l":"pl.put.poznan.JsonTools.rest"}];updateSearchResults();
